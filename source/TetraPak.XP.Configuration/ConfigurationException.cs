3b5652d1124e9347c075aa4bafcba491aca5deae
Jonas Rembratt <jonas.rembratt@tetrapak.com>
2022-02-15 13:20:26 +0100

    Added ConfigurationException

---
+++ source/TetraPak.XP.Configuration/ConfigurationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TetraPak.XP.Configuration
+{
+    /// <summary>
+    ///   Represents errors pertaining to configuration issues.
+    /// </summary>
+    public class ConfigurationException : Exception
+    {
+        public ConfigurationException(string message, Exception? inner = null)
+        : base(message, inner)
+        {
+        }
+    }
+}
