4266a7bd6f500fc0d0f060433b0431ecd27f08ba
Jonas Rembratt <jonas.rembratt@tetrapak.com>
2022-05-01 15:04:44 +0200

    -- intermit

--- source/TetraPak.XP.Logging.Abstractions/LogEventArgs.cs
+++ source/TetraPak.XP.Logging.Abstractions/LogEventArgs.cs
@@ -53,48 +53,4 @@ namespace TetraPak.XP.Logging.Abstractions
             Timestamp = timestamp ?? XpDateTime.UtcNow;
         }
     }
-
-    public static class LogEventArgsHelper
-    {
-        /// <summary>
-        ///   (fluent api)<br/>
-        ///   Assigns the <see cref="LogEventArgs.Rank"/> property and returns <c>this</c>.
-        /// </summary>
-        public static LogEventArgs WithRank(this LogEventArgs args, LogRank rank)
-        {
-            args.Rank = rank;
-            return args;
-        }
-    }
-
-    public sealed class LogFormatOptions
-    {
-        public bool OmitTimestamp { get; set; }
-
-        public bool OmitSource { get; set; }
-
-        public bool OmitRank { get; set; }
-
-        public bool OmitPrefix { get; set; }
-
-        public bool OmitMessageId { get; set; }
-
-        public static LogFormatOptions Default => new()
-        {
-            OmitTimestamp = false,
-            OmitSource = false,
-            OmitRank = false,
-            OmitPrefix = false,
-            OmitMessageId = false
-        };
-    }
-
-    public static class LogFormatOptionsHelper
-    {
-        public static LogFormatOptions WithOmitTimestamp(this LogFormatOptions options, bool value)
-        {
-            options.OmitTimestamp = value;
-            return options;
-        }
-    }
 }
